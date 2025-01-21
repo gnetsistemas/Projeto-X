4bade7a6f2495547bccd79fd789c0ae5c40fa2bc
Fabricio Bastos <gnetservtec@gmail.com>
2025-01-21 11:06:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
+    numLimite = numLimite;
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', `Escolha um número entre 1 e ${numLimite}`);
 }
