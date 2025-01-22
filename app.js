b890f7baf29028cd72251cb4218d440a575aed9c
Gutobastos <fabricio.bastos.ba@gmail.com>
2025-01-22 18:28:09 -0300

    Reverst correção conflito velocidade fala 1.25

--- app.js
+++ app.js
@@ -6,15 +6,7 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-<<<<<<< HEAD
-<<<<<<< HEAD
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.25});
-=======
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.5});
->>>>>>> parent of 4351b44 (Ajuste velocidade de fala 1.3)
-=======
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.3});
->>>>>>> parent of df42489 (Ajuste velocidade de fala 1.25)
 }
 
 function exibirMensagemInicial() {
